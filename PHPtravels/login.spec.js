f6e559a112e99214ad6488b313120f61489ee7b9
JPMANUCHA <juampi_manucha@hotmail.com>
2025-03-06 17:11:25 -0300

    Agregando capturas a test de login

--- PHPtravels/login.spec.js
+++ PHPtravels/login.spec.js
@@ -2,12 +2,12 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from './loginPage';
 
 
-test('Login with correct User', async ({ page }) => {
+test ('Login with correct User', async ({ page }) => {
 
     
     const Login = new LoginPage(page)
 
     await Login.goToLoginPage()
     await Login.login('juampi_manucha@hotmail.com', 'Ingeniero2209')
-
+    await page.screenshot({ path: 'Correct_login.png' });
 })
