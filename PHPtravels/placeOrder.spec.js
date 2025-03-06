ddcaa09ae7ea145454e70366e36c477f300e2e8d
JPMANUCHA <juampi_manucha@hotmail.com>
2025-03-06 17:12:41 -0300

    Se agrega capturas a prueba de orden

--- PHPtravels/placeOrder.spec.js
+++ PHPtravels/placeOrder.spec.js
@@ -9,9 +9,12 @@ test ('Place Order', async ({page}) => {
     const Login = new LoginPage(page);
     const Dash = new Dashboard(page);
 
-    await Login.goToLoginPage()
-    await Login.login('juampi_manucha@hotmail.com', 'Ingeniero2209')
+    await Login.goToLoginPage();
+    await Login.login('juampi_manucha@hotmail.com', 'Ingeniero2209');
+    await page.screenshot({ path: 'Login_OrderTest.png' });
     await Dash.goToDashboard();
+    await page.screenshot({ path: 'Dashboard.png' });
     await Dash.clickOrder(page);
+    await page.screenshot({ path: 'Order.png' });
 
 })
